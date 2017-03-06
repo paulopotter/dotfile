430ca50d44eeb7ae13d8636df51b0381aaa32aa9
Paulo Oliveira <paulofrauches@gmail.com>
2017-03-06 16:07:43 -0300

    check if installed before install

--- utils.sh
+++ utils.sh
@@ -2,47 +2,51 @@
 
 function install_package_with() {
   printf "\n"
+  if [ $(check_installed $1 $2) ]; then
+    if [[ $1 == "brew" ]]; then
+        echo "Instalando < $2 > com brew..."
+        brew install $2
+        echo "< $2 > instalado"
 
-  if [[ $1 == "brew" ]]; then
-      echo "Instalando < $2 > com brew..."
-      brew install $2
-      echo "< $2 > instalado"
+    elif [[ $1 == "cask" ]]; then
+        echo "Instalando < $2 > com brew cask..."
+        brew cask install $2
+        echo "< $2 > instalado"
 
-  elif [[ $1 == "cask" ]]; then
-      echo "Instalando < $2 > com brew cask..."
-      brew cask install $2
-      echo "< $2 > instalado"
+    elif [[ $1 == "pip" ]]; then
+        echo "Instalando/Atualizando < $2 > com pip..."
+        sudo pip install -U $2
+        echo "< $2 > instalado/atualizado"
 
-  elif [[ $1 == "pip" ]]; then
-      echo "Instalando/Atualizando < $2 > com pip..."
-      sudo pip install -U $2
-      echo "< $2 > instalado/atualizado"
+    elif [[ $1 == "apt" ]]; then
+        echo "Instalando < $2 > com apt-get..."
+        sudo apt-get install $2 -y
+        echo "< $2 > instalado"
 
-  elif [[ $1 == "apt" ]]; then
-      echo "Instalando < $2 > com apt-get..."
-      sudo apt-get install $2 -y
-      echo "< $2 > instalado"
+    elif [[ $1 == "aptitude" ]]; then
+        echo "Instalando < $2 > com aptitude..."
+        sudo aptitude install $2 -y
+        echo "< $2 > instalado"
 
-  elif [[ $1 == "aptitude" ]]; then
-      echo "Instalando < $2 > com aptitude..."
-      sudo aptitude install $2 -y
-      echo "< $2 > instalado"
+    elif [[ $1 == "dpkg" ]]; then
+        echo "Instalando < $2 > com dpkg..."
+        sudo dpkg -i $2
+        echo "< $2 > instalado"
 
-  elif [[ $1 == "dpkg" ]]; then
-      echo "Instalando < $2 > com dpkg..."
-      sudo dpkg -i $2
-      echo "< $2 > instalado"
+    elif [[ $1 == "gem" ]]; then
+        echo "Instalando < $2 > com gem..."
+        sudo gem install $1
+        echo "< $2 > instalado"
 
-  elif [[ $1 == "gem" ]]; then
-      echo "Instalando < $2 > com gem..."
-      sudo gem install $1
-      echo "< $2 > instalado"
+    elif [[ $1 == "npm" ]]; then
+        echo "Instalando < $2 > com npm..."
+        sudo npm install -g $1
+        echo "< $2 > instalado"
 
-  elif [[ $1 == "npm" ]]; then
-      echo "Instalando < $2 > com npm..."
-      sudo npm install -g $1
-      echo "< $2 > instalado"
+    fi
 
+  else
+    echo "< $2 > already installed"
   fi
 
   printf "\n"
@@ -63,4 +67,10 @@ function git_config(){
   git config --global user.email "$EMAIL"
   git config --global core.editor "$EDITOR"
   echo 'Configurado!'
+}
+
+function check_installed(){
+  if [ $1 == "apt" ] || [ $1 == "aptitude" ]; then
+    dpkg-query -Wf'${db:Status-abbrev}' "$2" 2>/dev/null | grep -q '^i'; 
+  fi
 }
