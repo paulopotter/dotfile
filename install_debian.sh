773ddec275f05ab5a0c33862013cf4978eec0db4
Paulo Oliveira <paulofrauches@example.com>
2017-03-09 08:11:56 -0300

    verify if spotify alredy installed

--- install_debian.sh
+++ install_debian.sh
@@ -96,8 +96,10 @@ function install_gnomeshell_extension_manager(){
 }
 
 function add_spotify_repository(){
+  if [[ ! $(check_installed "apt" "spotify-client") ]] ; then
     sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D2C19886
     echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+  fi
 }
 
 function install_package_control(){
